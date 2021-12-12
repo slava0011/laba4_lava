e5c2d084f7435f92a2bfa716063a69aa30f37712
slava0011 <mr.slawa2003@mail.ru>
2021-12-12 17:41:49 +0300

    added default variables

--- src/bsu/rfe/java/group7/lab4/Kondrashov/varB7/GraphicsDisplay.java
+++ src/bsu/rfe/java/group7/lab4/Kondrashov/varB7/GraphicsDisplay.java
@@ -1,4 +1,39 @@
 package bsu.rfe.java.group7.lab4.Kondrashov.varB7;
 
-public class GraphicsDisplay {
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.Stroke;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import javax.swing.JPanel;
+@SuppressWarnings("serial")
+public class GraphicsDisplay extends JPanel {
+    // Список координат точек для построения графика
+    private Double[][] graphicsData;
+    // Флаговые переменные, задающие правила отображения графика
+    private boolean showAxis = true;
+    private boolean showMarkers = true;
+    private boolean showIntGraphics = false;
+    // Границы диапазона пространства, подлежащего отображению
+    private double minX;
+    private double maxX;
+    private double minY;
+    private double maxY;
+    // Используемый масштаб отображения
+    private double scale;
+    // Различные стили черчения линий
+    private BasicStroke graphicsStroke;
+    private BasicStroke axisStroke;
+    private BasicStroke markerStroke;
+    private BasicStroke graphicsIntStroke;
+    // Различные шрифты отображения надписей
+    private Font axisFont;
 }
