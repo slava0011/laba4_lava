10f51d898bfcc8a505b0a6a42afafdbc8485c2e4
slava0011 <mr.slawa2003@mail.ru>
2021-12-12 17:56:40 +0300

    added openGraphics

--- src/bsu/rfe/java/group7/lab4/Kondrashov/varB7/MainFrame.java
+++ src/bsu/rfe/java/group7/lab4/Kondrashov/varB7/MainFrame.java
@@ -108,5 +108,49 @@ public class MainFrame extends JFrame {
         getContentPane().add(display, BorderLayout.CENTER);
     }
 
-    
+    protected void openGraphics(File selectedFile) {
+        try {
+            // Шаг 1 - Открыть поток чтения данных, связанный с входным файловым потоком
+            DataInputStream in = new DataInputStream(new
+                    FileInputStream(selectedFile));
+            /* Шаг 2 - Зная объѐм данных в потоке ввода можно вычислить,
+             * сколько памяти нужно зарезервировать в массиве:
+             * Всего байт в потоке - in.available() байт;
+             * Размер одного числа Double - Double.SIZE бит, или Double.SIZE/8 байт;
+             * Так как числа записываются парами, то число пар меньше в 2 раза
+             */
+            Double[][] graphicsData = new
+                    Double[in.available()/(Double.SIZE/8)/2][];
+            // Шаг 3 - Цикл чтения данных (пока в потоке есть данные)
+            int i = 0;
+            while (in.available()>0) {
+                // Первой из потока читается координата точки X
+                Double x = in.readDouble();
+                // Затем - значение графика Y в точке X
+                Double y = in.readDouble();
+                // Прочитанная пара координат добавляется в массив
+                graphicsData[i++] = new Double[] {x, y};
+            }
+            // Шаг 4 - Проверка, имеется ли в списке в результате чтения хотя бы одна пара координат
+            if (graphicsData!=null && graphicsData.length>0) {
+                // Да - установить флаг загруженности данных
+                fileLoaded = true;
+                // Вызывать метод отображения графика
+                display.showGraphics(graphicsData);
+            }
+            // Шаг 5 - Закрыть входной поток
+            in.close();
+        } catch (FileNotFoundException ex) {
+            // В случае исключительной ситуации типа "Файл не найден" показать сообщение об ошибке
+            JOptionPane.showMessageDialog(MainFrame.this, "Указанный " +
+                    "файл не найден", "Ошибка загрузки данных", JOptionPane.WARNING_MESSAGE);
+            return;
+        } catch (IOException ex) {
+            // В случае ошибки ввода из файлового потока показать сообщение об ошибке
+            JOptionPane.showMessageDialog(MainFrame.this, "Ошибка чтения " +
+                            "координат точек из файла", "Ошибка загрузки данных",
+                    JOptionPane.WARNING_MESSAGE);
+            return;
+        }
+    }
 }
