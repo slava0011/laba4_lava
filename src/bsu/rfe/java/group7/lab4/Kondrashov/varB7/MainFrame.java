db53c74360a4014bf59f64af7f4c12a9242159a9
slava0011 <mr.slawa2003@mail.ru>
2021-12-12 17:57:47 +0300

    added main and GraphicsMenuListener

--- src/bsu/rfe/java/group7/lab4/Kondrashov/varB7/MainFrame.java
+++ src/bsu/rfe/java/group7/lab4/Kondrashov/varB7/MainFrame.java
@@ -153,4 +153,27 @@ public class MainFrame extends JFrame {
             return;
         }
     }
+
+    public static void main(String[] args) {
+        // Создать и показать экземпляр главного окна приложения
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+    // Класс-слушатель событий, связанных с отображением меню
+    private class GraphicsMenuListener implements MenuListener {
+        // Обработчик, вызываемый перед показом меню
+        public void menuSelected(MenuEvent e) {
+            // Доступность или недоступность элементов меню "График" определяется загруженностью данных
+            showAxisMenuItem.setEnabled(fileLoaded);
+            showMarkersMenuItem.setEnabled(fileLoaded);
+            showIntGraphicsMenuItem.setEnabled(fileLoaded);
+        }
+        // Обработчик, вызываемый после того, как меню исчезло с экрана
+        public void menuDeselected(MenuEvent e) {
+        }
+        // Обработчик, вызываемый в случае отмены выбора пункта меню (очень редкая ситуация)
+        public void menuCanceled(MenuEvent e) {
+        }
+    }
 }
